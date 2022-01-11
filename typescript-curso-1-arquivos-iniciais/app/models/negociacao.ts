dddd1399e61ef37573ccc635b9abd0316eec31e1
BRENO@Breno <brenobrito_13@hotmail.com>
2022-01-11 19:31:48 -0300

    ajustando TS

--- typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,9 +1,9 @@
 export class Negociacao {
-    private _data;
-    private _quantidade;
-    private _valor;
+    private _data: Date;
+    private _quantidade: number;
+    private _valor: number;
 
-    constructor(data, quantidade, valor) {
+    constructor(data: Date, quantidade: number, valor: number) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
@@ -19,7 +19,7 @@ export class Negociacao {
         return this._valor;
     }
     get volume(){
-        return this.#quantidade * this.#valor;
+        return this._quantidade * this._valor;
     }
 
 }
