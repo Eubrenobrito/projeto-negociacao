7130bafbf9663db3e49d5d60ab0b6449a26ce040
BRENO@Breno <brenobrito_13@hotmail.com>
2022-01-28 17:26:07 -0300

    adicionando table, tr e td

--- typescript-curso-1-arquivos-iniciais/app/views/negociacoes-view.ts
+++ typescript-curso-1-arquivos-iniciais/app/views/negociacoes-view.ts
@@ -1,3 +1,5 @@
+import {Negociacoes} from "../models/negociacoes.js";
+
 export class NegociacoesView {
 
     private elemento: HTMLElement;
@@ -6,24 +8,35 @@ export class NegociacoesView {
         this.elemento = document.querySelector(seletor);
     }
     //o metodo template serve para declarar da view
-    template(): string {
+    template(model: Negociacoes): string {
         return `
            <table class="table table-hover table-bordered">
                 <thead>
                     <tr>
                         <th>DATA</th>
-                        <th>QUANTIDADE</th>
+                        <th class="text-center">QUANTIDADE</th>
                         <th class="text-right">VALOR</th>
                     </tr>
                 </thead>
                 <tbody>
-                
+                    ${model.lista().map(negociacao =>{
+                        return `
+                            <tr class="text-center">
+                                <td >${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                                <td >${negociacao.quantidade}</td>
+                                <td >${negociacao.valor}</td>
+                            </tr>
+                        `;
+                    }).join('')}
+                       
                 </tbody>
            </table> 
            `;
     }
     // método update serve para renderizar o template no elemento que foi capturado atraves do construtor(seletor)
-    update(): void {
-        this.elemento.innerHTML = this.template();
+    update(model: Negociacoes): void {
+        const template =this.template(model);
+        console.log(template);
+        this.elemento.innerHTML = template;
     }
 }
--- typescript-curso-1-arquivos-iniciais/dist/js/views/negociacoes-view.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/views/negociacoes-view.js
@@ -3,24 +3,35 @@ export class NegociacoesView {
         this.elemento = document.querySelector(seletor);
     }
     //o metodo template serve para declarar da view
-    template() {
+    template(model) {
         return `
            <table class="table table-hover table-bordered">
                 <thead>
                     <tr>
                         <th>DATA</th>
-                        <th>QUANTIDADE</th>
+                        <th class="text-center">QUANTIDADE</th>
                         <th class="text-right">VALOR</th>
                     </tr>
                 </thead>
                 <tbody>
-                
+                    ${model.lista().map(negociacao => {
+            return `
+                            <tr class="text-center">
+                                <td >${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                                <td >${negociacao.quantidade}</td>
+                                <td >${negociacao.valor}</td>
+                            </tr>
+                        `;
+        }).join('')}
+                       
                 </tbody>
            </table> 
            `;
     }
     // método update serve para renderizar o template no elemento que foi capturado atraves do construtor(seletor)
-    update() {
-        this.elemento.innerHTML = this.template();
+    update(model) {
+        const template = this.template(model);
+        console.log(template);
+        this.elemento.innerHTML = template;
     }
 }
